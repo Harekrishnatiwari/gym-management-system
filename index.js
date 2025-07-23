fd7a0d7382c6f22310a4ab3b1c1a5659e9a21ac5
HAREKRISHNA TIWARI <98546926+Harekrishnatiwari@users.noreply.github.com>
2025-07-23 12:24:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,42 @@ root.render(
   <App />
   </BrowserRouter>
    
-  </React.StrictMode>
+  </React.StrictMode>const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
+app.use(cookieParser());
+app.use(express.json());
+
+require('./DBConn/conn');
+
+// Routes
+const GymRoutes = require('./Routes/gym');
+const MembershipRoutes = require('./Routes/membership');
+const MemberRoutes = require('./Routes/member');
+
+app.use('/auth', GymRoutes);
+app.use('/plans', MembershipRoutes);
+app.use('/members', MemberRoutes);
+
+// Optional error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+});
+
+app.listen(PORT, () => {
+    console.log(`🎉 Server is running on port ${PORT}`);
+});
+
 );
 
 // If you want to start measuring performance in your app, pass a function
