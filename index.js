ecf20a12e0bbb357759732c9162e27d8ec1c8d58
HAREKRISHNA TIWARI <98546926+Harekrishnatiwari@users.noreply.github.com>
2025-07-23 12:16:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
-const express = require('express');
-const app = express();
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-require('dotenv').config();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const PORT = process.env.PORT || 5000;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+   
+  </React.StrictMode>
+);
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
-app.use(cookieParser());
-app.use(express.json());
-
-require('./DBConn/conn');
-
-// Routes
-const GymRoutes = require('./Routes/gym');
-const MembershipRoutes = require('./Routes/membership');
-const MemberRoutes = require('./Routes/member');
-
-app.use('/auth', GymRoutes);
-app.use('/plans', MembershipRoutes);
-app.use('/members', MemberRoutes);
-
-// Optional error handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Internal Server Error');
-});
-
-app.listen(PORT, () => {
-    console.log(`🎉 Server is running on port ${PORT}`);
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
